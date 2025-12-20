3ff5343e6db390e166a38c585c842c6e8468f691
Janvi1205 <janviyadav120505@gmail.com>
2025-12-21 02:55:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+
+const App=()=>{
+
+  return (
+    <div className="mx-auto max-w-[830px]">
+
+       <Navbar/>
+    </div>
+    
+
+
+  )
+
+
+}
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+import { CgProfile } from 'react-icons/cg'
+
+const Navbar=()=>{
+    return(
+        <div className='bg-white h-[60px]  flex  justify-center items-center gap-3  rounded-2xl mt-3'>
+            <CgProfile className='text-2xl'/>
+            <p className='text-xl font-bold'>Firebase Contact App</p>
+        </div>
+
+
+    );
+}
+export default Navbar;
